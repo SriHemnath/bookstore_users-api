89088d3e136fa637f288db23bd476c1a5fe6a19d
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2021-10-27 15:03:20 +0530

    updated error and date

---
+++ utils/date_utils/date_utils.go
@@ -0,0 +1,17 @@
+package date_utils
+
+import (
+	"time"
+)
+
+const (
+	apiDateLayout = "25-01-2006T15:04:05Z" //DDMMYYY
+)
+
+func GetNow() time.Time {
+	return time.Now().UTC() //setting standard utc time for all timezones
+}
+
+func GetNowString() string {
+	return time.Now().UTC().Format(apiDateLayout)
+}
